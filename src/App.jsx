f25e2f71eaa1c182a626ef20e76307d267ce38de
akik-hossain <akikhs00@gmail.com>
2023-10-07 02:02:59 +0600

    set environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold text-center bg-slate-400">
+        Knowledge Cafe
+      </h1>
+    </>
+  );
+}
+
+export default App;
