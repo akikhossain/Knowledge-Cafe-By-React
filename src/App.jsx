8d5d887b277bf4450f9d166343781b7be43437ed
akik-hossain <akikhs00@gmail.com>
2023-10-07 21:28:14 +0600

    added json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Blogs from "./Components/Blogs/Blogs";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl font-bold text-center bg-slate-400">
-        Knowledge Cafe
-      </h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-center bg-slate-400">
+        Knowledge Hub
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
