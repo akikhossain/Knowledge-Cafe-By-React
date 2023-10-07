777a61ca6d8300ee59e5a6f55e7e727bee19cb5f
akik-hossain <akikhs00@gmail.com>
2023-10-07 22:55:20 +0600

    added google fonts, images

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
+import profile from "../../assets/images/profile.png";
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-6xl font-bold text-center bg-slate-400">
-        Knowledge Hub
-      </h1>
+    <div className="flex justify-between">
+      <h1 className="text-4xl font-bold">Knowledge Hub</h1>
+      <img src={profile} alt="" />
     </div>
   );
 };
